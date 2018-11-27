692bde3f9548ddc366485a0fdeba2d8830aaeb43
Ljupka Filipovic <ljupka.f@gmail.com>
2018-11-27 02:49:03 +0100

    Feature added: Setting radar_types and connected them with specific url; Added function for downloading url and collecting paths of images in a list

--- hidmet_image_getter.py
+++ hidmet_image_getter.py
@@ -1,9 +1,33 @@
 import logging
 import argparse
+import urllib.request
+from bs4 import BeautifulSoup
+import re
 
 DEFAULT_SAVE_DIRECTORY = '/composite'
 DEFAULT_RADAR_TYPE = 'composite'
 DEFAULT_LOG_LEVEL = 'verbose'
+
+URL_FOR_KOSUTNJAK = "http://www.hidmet.gov.rs/ciril/osmotreni/radarska3bg.php"
+URL_FOR_LAWR = "http://www.hidmet.gov.rs/ciril/osmotreni/lawr2.php"
+URL_FOR_COMPOSITE = "http://www.hidmet.gov.rs/ciril/osmotreni/radarska3.php"
+
+# Setting radar types and their url
+radar_type = {'kosutnjak': URL_FOR_KOSUTNJAK, 'lawr': URL_FOR_LAWR, 'composite': URL_FOR_COMPOSITE}
+
+def downloading (url):
+    """
+    Downloading images from URL and collect their path in a list
+    """
+    data = urllib.request.urlopen(url).read()
+    soup = BeautifulSoup(data, features='lxml')
+    if url == URL_FOR_KOSUTNJAK or url == URL_FOR_COMPOSITE:
+        links = soup.findAll('div', id = 'slider1', src = False)
+        images = [i for i in links]
+    else:
+        links = soup.findAll('div', style='padding: 0 20px 0 20px;')
+        images = [i for i in links]
+    return print(images)
 
 if __name__ == "__main__":
 
@@ -11,7 +35,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--radar-type', help='says which radar is used in program', default=DEFAULT_RADAR_TYPE)
     parser.add_argument('--save-folder', help='place where the images are stored', default=DEFAULT_SAVE_DIRECTORY)
-    parser.add_argument('--log-level', help='increase output verbosity', default=DEFAULT_LOG_LEVEL)
+    parser.add_argument('--log-level', help='define log level', default=DEFAULT_LOG_LEVEL)
 
     args = parser.parse_args()
 
@@ -33,3 +57,6 @@ if __name__ == "__main__":
     print("Log level: " + args.log_level)
     print("Radar type:" + args.radar_type)
     print("Save folder:" + args.save_folder)
+
+    logging.info('Starting downloading images path from URL')
+    downloading(radar_type['kosutnjak'])
